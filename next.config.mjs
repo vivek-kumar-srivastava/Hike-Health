6b70edff48f7cf02f7a00b3c15d5ec3c22738593
vivek-kumar-srivastava <vivekisonline31@gmail.com>
2025-08-16 08:33:58 +0530

    Update message

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',        // 🔥 static export
-  images: {
-    unoptimized: true      // required for next/image in static export
-  }
-};
+
+}
 
 export default nextConfig;
